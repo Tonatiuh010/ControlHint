2df2409490c4f78221cbd6808eed9a5f47ac651a
Tonatiuh010 <l.tonatiuh010@gmail.com>
2022-11-13 22:16:31 -0800

    Fixing styles

--- src/FlowClient/src/app/layouts/footer/footer.component.ts
+++ src/FlowClient/src/app/layouts/footer/footer.component.ts
@@ -10,14 +10,15 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
   }
+
   scrollTop()
   {
-    window.scroll({ 
-      top: 0, 
-      left: 0, 
-      behavior: 'smooth' 
-});
-
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
   }
 }
